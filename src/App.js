d91895eb1372431f0504a5f8c955eff9c8cb7a22
manoj bisht <manoj1.bisht@paytm.com>
2022-01-06 19:12:02 +0530

    react single page with REST API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,69 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import { Cardlist } from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
+class App extends Component{
+  constructor(){
+    super();
 
+    this.state = {
+      monsters:[],
+      searchField:''
+    }
+   
+
+  }
+  componentDidMount(){
+    
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters:users}))
+    
+    /*
+    const monster = async () => {
+      try{
+      const userRespose = await fetch('https://jsonplaceholder.typicode.com/users');
+      const user = await userRespose.json();
+      const firstUser = user[0].id;
+      console.log(firstUser)
+      const userData = await fetch('https://jsonplaceholder.typicode.com/posts?userId='+firstUser);
+      const userDataResponse = await userData.json();
+      console.log(userDataResponse);
+      }
+      catch(err){
+        console.log('there was an error')
+      }
+
+    }
+    */
+
+  }
+
+  handleSearch = e => this.setState({searchField:e.target.value})
+
+  render(){
+     const {monsters, searchField} = this.state;
+     const filterSearch = monsters.filter( e => e.name.toLowerCase().includes(searchField.toLowerCase()))
+
+    return(
+    <div className="App">
+    <h1>Monsters Rolodex</h1>
+    <SearchBox
+      placeholder="Search monsters"
+      handleChange={this.handleSearch}
+
+    />
+    <Cardlist monsters={filterSearch} />
+     
+    </div>
+    )
+
+  }
+
+
+}
+
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +84,5 @@ function App() {
     </div>
   );
 }
-
+*/
 export default App;
